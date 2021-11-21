e81196c0161e54ac6d9eb0e314562287d29edf96
Waitaya <youlike35120@gmail.com>
2021-11-21 17:18:50 +0700

    update sesstion

--- frontend/src/Component/Robbie.js
+++ frontend/src/Component/Robbie.js
@@ -1,8 +1,5 @@
 import React , {useState , useEffect } from "react";
-
-import {
- useHistory
-} from "react-router-dom";
+import {useHistory} from "react-router-dom";
 
 const RobbieRoom = [{
  RoomId : 20154 , 
